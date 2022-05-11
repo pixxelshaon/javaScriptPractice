88661e1663b085d0e61e21d819e076f51014c62d
pixxelshaon <saifulislamshaonapk@gmail.com>
2022-05-11 22:53:41 +0600

    assignment operators

--- assignmentOperators.js
+++ assignmentOperators.js
@@ -6,5 +6,13 @@ num2 = num1;
 num1 += num1;
 num1++;
 num2 = num1 + num2;
+console.log(num1, num2);
 
+num1 = 50;
+num2 = 100;
+console.log(num1, num2);
+
+let temp = num1;
+num1 = num2;
+num2 = temp;
 console.log(num1, num2);
