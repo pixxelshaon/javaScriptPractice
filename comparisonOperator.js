4fa556689c9fe81338a77cf34584be198f5256b2
pixxelshaon <saifulislamshaonapk@gmail.com>
2022-05-11 23:02:35 +0600

    comparison operator

--- comparisonOperator.js
+++ comparisonOperator.js
@@ -1,7 +1,8 @@
 // JavaScript Comparison Operator
-// > < >= <= ==== !==
+// > < >= <= === !==
 
 let price = 250;
+
 console.log(price > 300);
 console.log(price < 300);
 console.log(price >= 300);
